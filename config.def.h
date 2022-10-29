acd8cef38bcb39fc59b71490a9d1614029ffa287
Connor Worrell <chemicalcoal1@gmail.com>
2022-10-29 01:59:53 -0600

    Fullscreen keybinding changed to Super+Shift+F

--- config.def.h
+++ config.def.h
@@ -111,7 +111,7 @@ static const double accel_speed = 0.0;
 
 /* commands */
 static const char *termcmd[] = { "wtermfallback", NULL };
-static const char *menucmd[] = { "yofi", NULL };
+static const char *menucmd[] = { "yofi", "binapps", NULL };
 static const char *hidebar[] = { "killall", "-SIGUSR1", "waybar", NULL };
 static const char *setwallpaper[] = { "setWallpaper", "-r" , NULL };
 static const char *lock[] = { "lock" , NULL };
@@ -168,7 +168,7 @@ static const Key keys[] = {
 	{ MODKEY,                    XKB_KEY_m,          setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                    XKB_KEY_space,      setlayout,      {0} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_space,      togglefloating, {0} },
-	{ MODKEY,                    XKB_KEY_e,         togglefullscreen, {0} },
+	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_F,         togglefullscreen, {0} },
 	{ MODKEY,                    XKB_KEY_0,          view,           {.ui = ~0} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_parenright, tag,            {.ui = ~0} },
 	{ MODKEY,                    XKB_KEY_comma,      focusmon,       {.i = WLR_DIRECTION_LEFT} },
