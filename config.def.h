28be3e2e7f1a7c8edf9278eff94a2c074ab98190
Connor Worrell <chemicalcoal1@gmail.com>
2022-11-05 20:51:16 -0600

    patched swallow

--- config.def.h
+++ config.def.h
@@ -22,16 +22,18 @@ static const int allow_constrain      = 1;
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-	/* app_id     title       tags mask     isfloating   monitor scratchkey */
+	/* app_id     title       tags mask     isfloating   isterm  noswallow  monitor scratchkey */
 	/* examples:
 	{ "Gimp",     NULL,       0,            1,           -1 },
 	*/
-	{ "firefox",  NULL,       1 << 8,       0,           -1,      0  },
-	{ NULL,     "termScratch",    0,            1,           -1,     'a' },
-	{ NULL,     "rangerScratch",  0,            1,           -1,     'b' },
-	{ NULL,     "calcScratch",    0,            1,           -1,     'c' },
-	{ NULL,     "volumeScratch",  0,            1,           -1,     'd' },
-	{ NULL,     "browserScratch", 0,            1,           -1,     'e' },
+	{ "firefox",  NULL,       1 << 8,       0,           0,      1,        -1,      0  },
+	{ NULL,     "termScratch",    0,        1,           0,      1,        -1,     'a' },
+	{ NULL,     "rangerScratch",  0,        1,           0,      1,        -1,     'b' },
+	{ NULL,     "calcScratch",    0,        1,           0,      1,        -1,     'c' },
+	{ NULL,     "volumeScratch",  0,        1,           0,      1,        -1,     'd' },
+	{ NULL,     "browserScratch", 0,        1,           0,      1,        -1,     'e' },
+	{ NULL,     "st",           0,        0,           1,      0,        -1,      0  },
+	{ "foot",     NULL,           0,        0,           1,      0,        -1,      0  },
 };
 
 /* layout(s) */
